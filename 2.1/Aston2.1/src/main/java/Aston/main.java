0306da90668792c5664892f6a9ad406181327fa9
Phoenix <phoenixreid@mail.ru>
2026-09-22 18:43:02 +0300

    Second commit

--- 2.1/Aston2.1/src/main/java/Aston/main.java
+++ 2.1/Aston2.1/src/main/java/Aston/main.java
@@ -146,7 +146,7 @@ public class main {
         int size = 7;
         int[][] matrix = new int[size][size];
 
-        for (int i = 0; i < a; i++){
+        for (int i = 0; i < size; i++) {
             matrix[i][i] = 1;
             matrix[i][size-1-i]=1;
         }
